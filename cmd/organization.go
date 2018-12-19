52fad75ab861316aa06802dbf54dba44b887d7e5
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-19 18:34:26 +0100

    Remove unused organization logic

--- cmd/organization.go
+++ cmd/organization.go
@@ -18,49 +18,11 @@ import (
 	"context"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	survey "gopkg.in/AlecAivazis/survey.v1"
+	"gopkg.in/AlecAivazis/survey.v1"
 )
 
 const orgIdKey = "organization.id"
-
-// organizationCmd represents the organization command
-var organizationCmd = &cobra.Command{
-	Use:     "organization",
-	Aliases: []string{"org", "orgs"},
-	Short:   "List and select organizations.",
-	Run:     OrganizationList,
-}
-
-var organizationSelectCmd = &cobra.Command{
-	Use:   "select",
-	Short: "Select organization.",
-	Run:   OrganizationSelect,
-	Args:  cobra.MaximumNArgs(1),
-}
-
-var organizationListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List organizations.",
-	Run:   OrganizationList,
-	Args:  cobra.NoArgs,
-}
-
-func OrganizationList(cmd *cobra.Command, args []string) {
-	pipeline := InitPipeline()
-	orgs, _, err := pipeline.OrganizationsApi.ListOrgs(context.Background())
-	if err != nil {
-		log.Fatalf("could not list organizations: %v", err)
-	}
-	Out(orgs, []string{"Id", "Name"})
-	id := GetOrgId(false)
-	for _, org := range orgs {
-		if org.Id == id {
-			log.Infof("Organization %q (%d) is selected.", org.Name, org.Id)
-		}
-	}
-}
 
 func searchOrganizationId(name string) int32 {
 	pipeline := InitPipeline()
@@ -76,29 +38,6 @@ func searchOrganizationId(name string) int32 {
 	}
 	log.Errorf("Could not find organization %q", name)
 	return 0
-}
-
-func searchOrganizationName(id int32) string {
-	pipeline := InitPipeline()
-	org, _, err := pipeline.OrganizationsApi.GetOrg(context.Background(), id)
-	if err != nil {
-		logAPIError("get organization", err, id)
-		return ""
-	}
-	return org.Name
-}
-
-func OrganizationSelect(cmd *cobra.Command, args []string) {
-	if len(args) == 1 {
-		id := searchOrganizationId(args[0])
-		if id > 0 {
-			viper.Set(orgIdKey, id)
-		}
-	} else {
-		viper.Set(orgIdKey, 0)
-		GetOrgId(true)
-	}
-	WriteConfig()
 }
 
 func GetOrgId(ask bool) int32 {
@@ -125,19 +64,4 @@ func GetOrgId(ask bool) int32 {
 		viper.Set(orgIdKey, id)
 	}
 	return id
-}
-
-func init() {
-	//rootCmd.AddCommand(organizationCmd)
-	//organizationCmd.AddCommand(organizationListCmd)
-	//organizationCmd.AddCommand(organizationSelectCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// organizationCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
