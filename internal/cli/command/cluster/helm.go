79c0a04d1feb1f0bb10fe3db54349633d4e81286
Mate Ory <mate.ory@banzaicloud.com>
2019-07-04 19:03:10 +0200

    cluster: download identified helm version

--- internal/cli/command/cluster/helm.go
+++ internal/cli/command/cluster/helm.go
@@ -15,14 +15,23 @@
 package cluster
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"runtime"
 	"strings"
 
 	"github.com/banzaicloud/banzai-cli/internal/cli"
 	"github.com/goph/emperror"
+	"github.com/mitchellh/go-homedir"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -46,6 +55,45 @@ func NewHelmCommand(banzaiCli cli.Cli) *cobra.Command {
 	return cmd
 }
 
+func writeHelm(url, name string) error {
+
+	tgz, err := http.Get(url)
+	if err != nil {
+		return emperror.Wrapf(err, "failed to download helm from %q", url)
+	}
+	defer tgz.Body.Close()
+
+	zr, err := gzip.NewReader(tgz.Body)
+	if err != nil {
+		return emperror.Wrap(err, "failed to uncompress helm archive")
+	}
+	defer zr.Close()
+
+	tr := tar.NewReader(zr)
+	for {
+		hdr, err := tr.Next()
+		if err != nil {
+			return emperror.Wrap(err, "failed to extract helm from archive")
+		}
+		if filepath.Base(hdr.Name) == "helm" {
+			tempName := name + "~"
+			f, err := os.OpenFile(tempName, (os.O_WRONLY | os.O_CREATE | os.O_EXCL), 0755)
+			if err != nil {
+				return emperror.Wrap(err, "failed to create temporary file for helm binary")
+			}
+
+			_, err = io.Copy(f, tr)
+			f.Close()
+
+			if err != nil {
+				return emperror.Wrap(err, "failed to write helm binary")
+			}
+
+			return emperror.Wrap(os.Rename(tempName, name), "failed to move helm binary to its final place")
+		}
+	}
+}
+
 func runHelm(banzaiCli cli.Cli, options helmOptions, args []string) error {
 	c := exec.Command("kubectl", "get", "deployment", "-n", "kube-system", "-o", "jsonpath={.items[0].spec.template.spec.containers[0].image}", "-l", "app=helm")
 	out, err := c.Output()
@@ -56,9 +104,32 @@ func runHelm(banzaiCli cli.Cli, options helmOptions, args []string) error {
 	if len(parts) != 2 {
 		return errors.Errorf("failed to parse Tiller image name %q", out)
 	}
-	version := parts[1]
+	version := parts[1] // TODO check format
 
-	fmt.Printf("https://get.helm.sh/helm-%s-%s-amd64.tar.gz", version, runtime.GOOS)
+	// TODO use dir from config
+	home, err := homedir.Dir()
+	if err != nil {
+		return emperror.Wrap(err, "can't compose the default config file path")
+	}
+
+	bindir := path.Join(home, ".banzai/bin")
+	if err := os.MkdirAll(bindir, 0755); err != nil {
+		return emperror.Wrapf(err, "failed to create %q directory", bindir)
+	}
+
+	url := fmt.Sprintf("https://get.helm.sh/helm-%s-%s-amd64.tar.gz", version, runtime.GOOS)
+	name := path.Join(bindir, fmt.Sprintf("helm-%s", version))
+
+	if _, err := os.Stat(name); err != nil {
+		log.Infof("Downloading helm %s...", version)
+		if runtime.GOARCH != "amd64" {
+			return errors.Errorf("unsupported architecture: %v", runtime.GOARCH)
+		}
+		if err := writeHelm(url, name); err != nil {
+			return emperror.Wrap(err, "failed to download helm client")
+		}
+		log.Infof("Helm %s downloaded successfully", version)
+	}
 
 	return nil
 }
