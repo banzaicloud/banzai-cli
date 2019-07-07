f8bb2caf9e6bc57d2c55ead537c54deafcf2d5d2
Zsolt Varga <zsolt.varga@console.hu>
2019-07-08 00:19:41 +0200

    Pipeline json error converter

--- internal/cli/utils/utils.go
+++ internal/cli/utils/utils.go
@@ -20,6 +20,9 @@ import (
 
 	"github.com/ghodss/yaml"
 	"github.com/goph/emperror"
+	"github.com/pkg/errors"
+
+	"github.com/banzaicloud/banzai-cli/.gen/pipeline"
 )
 
 func Unmarshal(raw []byte, data interface{}) error {
@@ -43,4 +46,23 @@ func Unmarshal(raw []byte, data interface{}) error {
 	}
 
 	return nil
+}
+
+// ConvertError converts generic HTTP error in JSON format returned by Pipeline API
+func ConvertError(err error) error {
+	type Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Error   string `json:"error"`
+	}
+
+	if gerr, ok := errors.Cause(err).(pipeline.GenericOpenAPIError); ok {
+		var pipelineError Error
+		e := json.Unmarshal(gerr.Body(), &pipelineError)
+		if e == nil {
+			return errors.WithMessage(err, pipelineError.Error)
+		}
+	}
+
+	return err
 }
