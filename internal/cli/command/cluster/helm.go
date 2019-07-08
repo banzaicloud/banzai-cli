7e1725edd14268d3c2bc38b6f16b9aa7f751e7e7
Mate Ory <mate.ory@banzaicloud.com>
2019-07-08 09:30:06 +0200

    shell: disable gosec check
    internal/cli/command/cluster/helm.go:61: G107: Potential HTTP request made with variable url (gosec)

--- internal/cli/command/cluster/helm.go
+++ internal/cli/command/cluster/helm.go
@@ -58,7 +58,7 @@ func NewHelmCommand(banzaiCli cli.Cli) *cobra.Command {
 
 func writeHelm(url, name string) error {
 
-	tgz, err := http.Get(url)
+	tgz, err := http.Get(url) // #nosec
 	if err != nil {
 		return emperror.Wrapf(err, "failed to download helm from %q", url)
 	}
