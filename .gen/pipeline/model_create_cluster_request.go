7fb6d98da92a1f4d01513fa868cb26fd2547acb0
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-09-03 11:38:55 +0200

    Regenerate pipeline client

--- .gen/pipeline/model_create_cluster_request.go
+++ .gen/pipeline/model_create_cluster_request.go
@@ -3,7 +3,7 @@
  *
  * Pipeline v0.3.0 swagger
  *
- * API version: 0.29.0-dev.1
+ * API version: 0.29.1
  * Contact: info@banzaicloud.com
  */
 
@@ -21,7 +21,6 @@ type CreateClusterRequest struct {
 	SecretIds []string `json:"secretIds,omitempty"`
 	SecretName string `json:"secretName,omitempty"`
 	PostHooks map[string]interface{} `json:"postHooks,omitempty"`
-	ProfileName string `json:"profileName,omitempty"`
 	ScaleOptions ScaleOptions `json:"scaleOptions,omitempty"`
 	Properties map[string]interface{} `json:"properties"`
 }
