7ff06402b38b731e44b4da857a0ce649cf71d70c
pepov <peterwilcsinszky@gmail.com>
2019-12-05 11:33:47 +0100

    fix license violations

--- scripts/check-header.sh
+++ scripts/check-header.sh
@@ -17,7 +17,7 @@ read -r -d '' EXPECTED <<EOF
 EOF
 
 STATUS=0
-FILES=$(find . -name "*.go" -not -path "./vendor/*" -not -path "./.gen/*")
+FILES=$(find . -name "*.go" -not -path "./vendor/*" -not -path "./.gen/*" -not -path "./internal/cli/utils/untar/untar.go")
 
 for FILE in $FILES; do
     # Replace the actual year with DATE so we can ignore the year when
